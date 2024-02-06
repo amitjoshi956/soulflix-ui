a0ba008a76423b75a8f37cba2163cc87706b005c
Amit Joshi <amitjoshi956@gmail.com>
2024-02-07 00:08:17 +0530

    Redux store setup and prettier config updates

---
+++ src/core/store/rootReducer.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    // Start adding reducers here
+});
+
+export default rootReducer;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { I18nextProvider } from 'react-i18next';
+import { Provider as StoreProvider } from 'react-redux';
 import i18n from '../i18n.config';
+import { store } from 'core/store';
 import App from './App';
 
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <I18nextProvider i18n={i18n}>
-            <App />
-        </I18nextProvider>
+        <StoreProvider store={store}>
+            <I18nextProvider i18n={i18n}>
+                <App />
+            </I18nextProvider>
+        </StoreProvider>
     </React.StrictMode>
 );
