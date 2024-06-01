042d3a12969d2518fc06d81218cf7ac732cc6e14
Amit Joshi <amitjoshi956@gmail.com>
2024-06-01 13:44:35 +0530

    Features/common components setup (#11)
    * Style updates
    
    * MovieCard component updates

--- src/components/custom/MovieCard/MovieCard.tsx
+++ src/components/custom/MovieCard/MovieCard.tsx
@@ -1,5 +1,22 @@
+import './MovieCard.scss';
+
 const MovieCard = () => {
-    return <div>MovieCard</div>;
+    return (
+        <div className="movie-card">
+            <div className="movie-card__preview">
+                <img
+                    className="movie-card__thumbnail"
+                    src="https://www.mayapur.com/wp-content/uploads/2017/10/RNS-1024x576.jpg"
+                    alt="movie-card"
+                />
+            </div>
+            <div className="movie-card__meta">
+                <p className="movie-card__title">Glories of Srila Gopal Bhatta Goswami</p>
+                <div className="movie-card__controls"></div>
+                <div className="movie-card__tags"></div>
+            </div>
+        </div>
+    );
 };
 
 export default MovieCard;
--- src/pages/Test/YTPlayer/TestYtPlayer.tsx
+++ src/pages/Test/YTPlayer/TestYtPlayer.tsx
@@ -1,14 +1,20 @@
 import YTPlayer from 'components/base/YTPlayer';
+import MovieCard from 'components/custom/MovieCard';
 
 import './TestYtPlayer.scss';
 const TestYtPlayer = () => {
     return (
-        <div className="test-yt-player">
-            <YTPlayer
-                videoId={'abmsniEmfh0'}
-                title={'Srila Prabhupada Interview - San Francisco'}
-            />
-        </div>
+        <>
+            <div className="test-yt-player">
+                <YTPlayer
+                    videoId={'abmsniEmfh0'}
+                    title={'Srila Prabhupada Interview - San Francisco'}
+                />
+            </div>
+            <div className="test-movie-card">
+                <MovieCard />
+            </div>
+        </>
     );
 };
 
