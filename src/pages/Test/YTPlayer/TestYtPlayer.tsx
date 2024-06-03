520611054f9c64dd766b57da78427c2ae2e4fe6f
Amit Joshi <amitjoshi956@gmail.com>
2024-06-03 23:22:53 +0530

    Fixing build error

--- src/pages/Test/YTPlayer/TestYtPlayer.tsx
+++ src/pages/Test/YTPlayer/TestYtPlayer.tsx
@@ -1,7 +1,5 @@
-import { MdAutoMode } from 'react-icons/md';
 import YTPlayer from 'components/base/YTPlayer';
 import MovieCard from 'components/custom/MovieCard';
-import Button from 'components/base/Button';
 
 import './TestYtPlayer.scss';
 const TestYtPlayer = () => {
