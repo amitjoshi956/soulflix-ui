ab9cb55b63884c21e8fdf735059ee46e09c9ac7d
Amit Joshi <amitjoshi956@gmail.com>
2024-06-09 19:12:19 +0530

    Common Component changes (#15)
    * Updates in button component
    
    * MovieCard component updated
    
    * Button component style updates

--- src/components/base/Button/Button.tsx
+++ src/components/base/Button/Button.tsx
@@ -8,33 +8,37 @@ type ButtonProps = {
     classname?: string;
     variant: 'default' | 'alt' | 'outlined' | 'text' | 'round-filled' | 'round-outlined';
     iconPlacement?: 'left' | 'right';
+    size?: 'small' | 'medium' | 'large';
     disabled?: boolean;
     Icon?: IconType;
     onClick?: () => void;
 };
 
 const Button: FC<ButtonProps> = ({
-    label,
+    label = '',
     classname = '',
     variant,
     iconPlacement = 'left',
+    size = 'medium',
     disabled = false,
     Icon,
     onClick,
 }) => {
-    const btnModifierClassSuffix = disabled ? 'disabled' : variant;
     const isNotRoundBtn = variant !== 'round-filled' && variant !== 'round-outlined';
 
     const btnContent = (
         <div className={`button__content button__content--icon-${iconPlacement}`}>
-            {Icon && <Icon className="button__icon" />}
-            {label && isNotRoundBtn && <span className="button__label">{label}</span>}
+            {Icon && <Icon className={`button__icon button__icon--${size}`} />}
+            {label && isNotRoundBtn && (
+                <span className={`button__label button__label--${size}`}>{label}</span>
+            )}
         </div>
     );
 
     return (
         <button
-            className={`button button--${btnModifierClassSuffix} ${classname}`}
+            className={`button button--${variant} ${classname}`.trim()}
+            disabled={disabled}
             aria-label={`${label} button`}
             onClick={onClick}
         >
--- src/pages/Test/Buttons/Buttons.tsx
+++ src/pages/Test/Buttons/Buttons.tsx
@@ -5,23 +5,127 @@ import './Buttons.scss';
 
 const Buttons = () => {
     return (
-        <div className="test-buttons">
-            <Button variant="default" label="Default button" Icon={MdAutoMode} />
-            <Button
-                variant="default"
-                disabled
-                label="Right icon button"
-                iconPlacement="right"
-                Icon={MdAutoMode}
-            />
-            <Button variant="alt" label="Alt button with some really huge name for this button" />
-            <Button
-                variant="outlined"
-                label="Outlined button some really huge name for this button"
-            />
-            <Button variant="text" label="Text button" />
-            <Button variant="round-filled" label="Round button" Icon={MdAutoMode} />
-            <Button variant="round-outlined" Icon={MdPlayArrow} />
+        <div className="test-buttons-container">
+            <h6>Buttons with labels</h6>
+            <div className="test-buttons">
+                <Button variant="default" label="Default button no icon" />
+                <Button variant="default" label="Left icon button" Icon={MdAutoMode} />
+                <Button
+                    variant="default"
+                    disabled
+                    label="Right icon button"
+                    iconPlacement="right"
+                    Icon={MdAutoMode}
+                />
+                <Button
+                    variant="alt"
+                    label="Alt button with some really huge name for this button"
+                />
+                <Button
+                    variant="outlined"
+                    label="Outlined button some really huge name for this button"
+                />
+                <Button variant="text" label="Text button" />
+            </div>
+            <h6>Default button with sizes</h6>
+            <div className="test-buttons">
+                <Button variant="default" size="small" label="Small button" />
+                <Button variant="default" disabled size="small" label="Small button Disabled" />
+                <Button variant="default" size="medium" label="Medium button" />
+                <Button variant="default" disabled size="medium" label="Medium button Disabled" />
+                <Button variant="default" size="large" label="Large button" />
+                <Button variant="default" disabled size="large" label="Large button Disabled" />
+            </div>
+            <h6>Alt button with sizes</h6>
+            <div className="test-buttons">
+                <Button variant="alt" size="small" label="Small Alt button" />
+                <Button variant="alt" disabled size="small" label="Small Alt button Disabled" />
+                <Button variant="alt" size="medium" label="Medium Alt button" />
+                <Button variant="alt" disabled size="medium" label="Medium Alt button Disabled" />
+                <Button variant="alt" size="large" label="Large Alt button" />
+                <Button variant="alt" disabled size="large" label="Large Alt button Disabled" />
+            </div>
+            <h6>Outlined button with sizes</h6>
+            <div className="test-buttons">
+                <Button variant="outlined" size="small" label="Small Outlined button" />
+                <Button
+                    variant="outlined"
+                    disabled
+                    size="small"
+                    label="Small Outlined button Disabled"
+                />
+                <Button variant="outlined" size="medium" label="Medium Outlined button" />
+                <Button
+                    variant="outlined"
+                    disabled
+                    size="medium"
+                    label="Medium Outlined button Disabled"
+                />
+                <Button variant="outlined" size="large" label="Large Outlined button" />
+                <Button
+                    variant="outlined"
+                    disabled
+                    size="large"
+                    label="Large Outlined button Disabled"
+                />
+            </div>
+            <h6>Text button with sizes</h6>
+            <div className="test-buttons">
+                <Button variant="text" size="small" label="Small Text button" />
+                <Button variant="text" disabled size="small" label="Small Text button Disabled" />
+                <Button variant="text" size="medium" label="Medium Text button" />
+                <Button variant="text" disabled size="medium" label="Medium Text button Disabled" />
+                <Button variant="text" size="large" label="Large Text button" />
+                <Button variant="text" disabled size="large" label="Large Text button Disabled" />
+            </div>
+            <h6>Round buttons - icon only</h6>
+            <div className="test-buttons">
+                <Button
+                    variant="round-filled"
+                    size="small"
+                    label="Round button"
+                    Icon={MdAutoMode}
+                />
+                <Button
+                    variant="round-filled"
+                    size="small"
+                    disabled
+                    label="Round button"
+                    Icon={MdAutoMode}
+                />
+                <Button
+                    variant="round-filled"
+                    size="medium"
+                    label="Round button"
+                    Icon={MdAutoMode}
+                />
+                <Button
+                    variant="round-filled"
+                    size="medium"
+                    disabled
+                    label="Round button"
+                    Icon={MdAutoMode}
+                />
+                <Button
+                    variant="round-filled"
+                    size="large"
+                    label="Round button"
+                    Icon={MdAutoMode}
+                />
+                <Button
+                    variant="round-filled"
+                    size="large"
+                    disabled
+                    label="Round button"
+                    Icon={MdAutoMode}
+                />
+                <Button variant="round-outlined" size="small" Icon={MdPlayArrow} />
+                <Button variant="round-outlined" disabled size="small" Icon={MdPlayArrow} />
+                <Button variant="round-outlined" Icon={MdPlayArrow} />
+                <Button variant="round-outlined" disabled Icon={MdPlayArrow} />
+                <Button variant="round-outlined" size="large" Icon={MdPlayArrow} />
+                <Button variant="round-outlined" disabled size="large" Icon={MdPlayArrow} />
+            </div>
         </div>
     );
 };
