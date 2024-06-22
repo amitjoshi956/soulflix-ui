058117efd91ca0b88bc97af7349c4af387a2ddf7
Amit Joshi <amitjoshi956@gmail.com>
2024-06-23 02:24:34 +0530

    Updates in SliderCard & YTPlayer components

--- src/components/base/YTPlayer/YTPlayer.tsx
+++ src/components/base/YTPlayer/YTPlayer.tsx
@@ -1,27 +1,43 @@
-import { FC } from 'react';
-import { PLAYER_PARAMS } from 'core/base/consts/ytPlayer';
+import { FC, useRef } from 'react';
+import { PLAYER_DEFAULT, PLAYER_PARAMS } from 'core/base/consts/ytPlayer';
+import { VideoParams } from 'core/base/types/ytPlayer';
 
 import './YTPlayer.scss';
 
-export type VideoParams = {
-    startTime?: number;
-    showRelatedVideos?: boolean;
-    showControls?: boolean;
-    autoplay?: boolean;
-    allowFullScreen?: boolean;
-};
-
 const generateSrc = (
     videoId: string,
-    { showControls, showRelatedVideos, startTime, autoplay }: VideoParams
+    {
+        showControls,
+        showRelatedVideos,
+        startTime,
+        endTime,
+        autoplay,
+        disableKeyboard,
+        loop = false,
+    }: VideoParams
 ) => {
-    const { BASE_SRC, REL, CONTROLS, AUTOPLAY_ON_LOAD, START_TIME } = PLAYER_PARAMS;
-    const rel = showRelatedVideos ? '1' : '0';
-    const controls = showControls ? '1' : '0';
-    const autoplayVideo = autoplay ? `&${AUTOPLAY_ON_LOAD}=1` : '';
-    const start = startTime ? `;${START_TIME}=${startTime}` : '0';
+    const {
+        BASE_SRC,
+        REL,
+        CONTROLS,
+        AUTOPLAY_ON_LOAD,
+        START_TIME,
+        END_TIME,
+        DISABLE_KEYBOARD,
+        LOOP,
+    } = PLAYER_PARAMS;
 
-    return `${BASE_SRC}/${videoId}?${REL}=${rel}&${CONTROLS}=${controls}${autoplayVideo}${start}`;
+    const { OFF, ON, START_AT, END_DURATION } = PLAYER_DEFAULT;
+
+    const rel = showRelatedVideos ? ON : OFF;
+    const controls = showControls ? ON : OFF;
+    const autoplayVideo = autoplay ? ON : OFF;
+    const start = startTime ?? START_AT;
+    const end = endTime ?? END_DURATION;
+    const shouldDisableKeyboard = disableKeyboard ? ON : OFF;
+    const loopVideo = loop ? ON : OFF;
+
+    return `${BASE_SRC}/${videoId}?${REL}=${rel}&${CONTROLS}=${controls}&${DISABLE_KEYBOARD}=${shouldDisableKeyboard}&${START_TIME}=${start}&${END_TIME}=${end}&${AUTOPLAY_ON_LOAD}=${autoplayVideo}&${LOOP}=${loopVideo}`.trim();
 };
 
 type YTPlayerProps = {
@@ -29,17 +45,39 @@ type YTPlayerProps = {
     videoId: string;
     title: string;
     playerParams?: VideoParams;
+    onPlay?: () => void;
+    onPause?: () => void;
+    onVolumeChange?: (volume: number) => void;
+    onMute?: () => void;
+    onUnmute?: () => void;
+    onSeek?: (moment: number) => void;
+    onForward10s?: () => void;
+    onRewind10s?: () => void;
+    onSpeedChange?: (speed: number) => void;
+    onShowFullscreen?: () => void;
+    onExitFullscreen?: () => void;
+    onReady?: () => void;
+    onEnded?: () => void;
 };
 
 const YTPlayer: FC<YTPlayerProps> = ({ className = '', videoId, title, playerParams = {} }) => {
+    const iframeRef = useRef<HTMLIFrameElement>(null);
+
+    const handleOnLoad = () => {
+        // TODO: Research on this and fix this
+        // if (iframeRef.current) console.log('$$$YT-Load =>', iframeRef.current.contentDocument);
+    };
+
     return (
         <div className={`yt-player ${className}`.trim()}>
             <iframe
                 className="yt-player__iframe"
+                ref={iframeRef}
                 src={generateSrc(videoId, playerParams)}
-                title={title}
+                aria-label={title}
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;"
                 allowFullScreen={!!playerParams.allowFullScreen}
+                onLoad={handleOnLoad}
             />
         </div>
     );
--- src/core/base/consts/ytPlayer.ts
+++ src/core/base/consts/ytPlayer.ts
@@ -4,4 +4,14 @@ export const PLAYER_PARAMS = {
     CONTROLS: 'controls',
     AUTOPLAY_ON_LOAD: 'autoplay',
     START_TIME: 'start',
+    END_TIME: 'end',
+    DISABLE_KEYBOARD: 'disablekb',
+    LOOP: 'loop',
+};
+
+export const PLAYER_DEFAULT = {
+    OFF: 0,
+    ON: 1,
+    START_AT: 0,
+    END_DURATION: 60,
 };
