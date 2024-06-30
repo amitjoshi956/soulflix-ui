d5de052ca26162f1acc650f8e8e46bcf135d96d3
Amit Joshi <amitjoshi956@gmail.com>
2024-07-01 01:52:07 +0530

    AppHeader component & routing changes (#19)

--- src/core/base/consts/appHeader.ts
+++ src/core/base/consts/appHeader.ts
@@ -1,12 +1,32 @@
+import { t } from 'i18next';
 import { NavigationItem } from '../types/appHeader';
+import { Routes } from './routes';
+
+const getName = (key: string) => t(`appHeader.${key}`, { ns: 'common' });
 
 export const NavigationConfig: NavigationItem[] = [
     {
-        name: 'Home',
-        link: '/',
+        name: getName('home'),
+        link: Routes.HOME,
     },
     {
-        name: 'My List',
-        link: '/my-list',
+        name: getName('movies'),
+        link: Routes.MOVIES,
+    },
+    {
+        name: getName('series'),
+        link: Routes.SERIES,
+    },
+    {
+        name: getName('podcasts'),
+        link: Routes.PODCASTS,
+    },
+    {
+        name: getName('watchlist'),
+        link: Routes.WATCHLIST,
+    },
+    {
+        name: 'Test',
+        link: Routes.TEST,
     },
 ];
---
+++ src/core/base/consts/routes.ts
@@ -0,0 +1,8 @@
+export const Routes = {
+    HOME: '/',
+    MOVIES: '/movies',
+    SERIES: '/series',
+    PODCASTS: '/podcasts',
+    WATCHLIST: '/watch-list',
+    TEST: '/test',
+};
