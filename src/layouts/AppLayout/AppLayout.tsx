f0f23b5ec2a3c2be9099daebbcdec7d5307dd474
Amit Joshi <amitjoshi956@gmail.com>
2024-07-21 19:36:19 +0530

    Features/common components setup (#24)

--- src/layouts/AppLayout/AppLayout.tsx
+++ src/layouts/AppLayout/AppLayout.tsx
@@ -1,7 +1,16 @@
 import AppHeader from 'components/custom/AppHeader';
 import Billboard from 'components/custom/Billboard';
+import Slider from 'components/custom/Slider';
 
 import './AppLayout.scss';
+
+const sliderData = Array.from({ length: 20 }, (_, index) => ({
+    id: `vid-${index}`,
+    videoId: 'abmsniEmfh0',
+    title: `Test video ${index + 1}`,
+    thumbnail: `https://picsum.photos/200/300?random=${index + 1}`,
+    tags: ['tag1', 'tag2', 'tag3'],
+}));
 
 const AppLayout = () => {
     return (
@@ -20,7 +29,9 @@ const AppLayout = () => {
                     // TODO: implement logic
                 }}
             />
-            <div style={{ height: '100vh' }}></div>
+            <div style={{ height: '100vh', position: 'relative', zIndex: 20, marginTop: '-190px' }}>
+                <Slider title="Trending" videos={sliderData} totalCount={sliderData.length} />
+            </div>
         </div>
     );
 };
